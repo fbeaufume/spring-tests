18fc594d1e63335e1c32de2f1117919b088e4e7c
Stéphane Nicoll <stephane.nicoll@broadcom.com>
2024-03-19 09:15:25 +0100

    Make profile switching more idiomatic
    This commit removes the profile resolver and use core Spring Boot
    features to achieve a similar, more idiomatic result.
    
    Rather than making a profile mandatory for tests, we flip the
    responsibility so that everything is provided with the default
    configuration, and an `app` profile provides a specific URL for the
    H2 datasource of the application. Technically, the URL in
    application.properties should be completely removed to rely on the
    auto-configuration.
    
    Enabling a profile on the command line can be achieved in a more
    elegant way by passing the property to the surefire plugin, reusing the
    existing `test.profiles` property.
    
    Finally, we need to make sure the app profile is enabled when the app
    starts and that can be easily achieved using SpringApplicationBuilder.

--- src/test/java/com/adeliosys/sample/BaseTest.java
+++ src/test/java/com/adeliosys/sample/BaseTest.java
@@ -5,12 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@ActiveProfiles(value = "test", resolver = CustomActiveProfilesResolver.class)
 public abstract class BaseTest {
 
     @Autowired
