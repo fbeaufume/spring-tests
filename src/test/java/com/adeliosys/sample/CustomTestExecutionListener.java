6c4e6497698d1257a2b2769a87b21b2340a1968b
Florian Beaufumé <fbeaufume@gmail.com>
2024-03-29 17:24:37 +0100

    Logged the tests duration using a custom JUnit listener

---
+++ src/test/java/com/adeliosys/sample/CustomTestExecutionListener.java
@@ -0,0 +1,28 @@
+package com.adeliosys.sample;
+
+import org.junit.platform.launcher.TestExecutionListener;
+import org.junit.platform.launcher.TestPlan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This class is declared in the META-INF/services/org.junit.platform.launcher.TestExecutionListener file.
+ * Note that Spring Test also provides a TestExecutionListener interface, but it is not the same as the one from JUnit.
+ */
+public class CustomTestExecutionListener implements TestExecutionListener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CustomTestExecutionListener.class);
+
+    private static long startTimestamp = 0;
+
+    @Override
+    public void testPlanExecutionStarted(TestPlan testPlan) {
+        startTimestamp = System.currentTimeMillis();
+    }
+
+    @Override
+    public void testPlanExecutionFinished(TestPlan testPlan) {
+        long duration = System.currentTimeMillis() - startTimestamp;
+        LOGGER.info("Tests executed in {} s", String.format("%d.%03d", duration / 1000, duration % 1000));
+    }
+}
